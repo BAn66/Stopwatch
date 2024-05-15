8b478d2d15e4d087f120c5cae0560a891b8adbed
Kostenko Konstantin <thetanqel@gmail.com>
2024-05-15 11:11:11 +0400

    main activity onSaveInstanceState

--- app/src/main/java/ru/kostenko/stopwatch/MainActivity.kt
+++ app/src/main/java/ru/kostenko/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.kostenko.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
@@ -11,11 +12,28 @@ class MainActivity : AppCompatActivity() {
     var runnig = false
     var offset: Long = 0
 
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        if(savedInstanceState != null){
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            runnig = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (runnig){
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            }
+            else{
+             setBaseTime()
+            }
+        }
 
         val startButton = findViewById<Button>(R.id.start_button)
         startButton.setOnClickListener{
@@ -43,6 +61,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, runnig)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
     fun setBaseTime(){
         stopwatch.base = SystemClock.elapsedRealtime() - offset
     }
@@ -50,4 +76,6 @@ class MainActivity : AppCompatActivity() {
     fun saveOffset(){
         offset = SystemClock.elapsedRealtime() - stopwatch.base
     }
+
+
 }
